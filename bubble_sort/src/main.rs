cd87153a1b9e4a058bb4a08409c3ac809967d815
zettaittenani <zettaittenani@gmail.com>
2018-02-19 22:16:31 +0900

    Impl bubble sort

--- bubble_sort/src/main.rs
+++ bubble_sort/src/main.rs
@@ -1,19 +1,18 @@
 fn main() {
-    let mut input: [i32; 5] = [5, 2, 4, 3, 1];
-    test(input);
-}
+    let input: [i32; 5] = [5, 2, 4, 3, 1];
+    let mut output = input.clone();
 
-fn test(input: [i32; 5]){
-    swap(&input[0], &input[1]);
-    println!("{}", input[0]);
-}
-
-fn swap<'a>(first: &'a mut i32, second: &'a mut i32){
-    let mut temp: &i32 = &0;
-    
-    if first > second {
-        temp = first;
-        first = second;
-        second = temp;
+    for m in 0..output.len() {
+        for n in 0..output.len() {
+            if output[m] < output[n] {
+                output.swap(m, n);
+            }
+        }
     }
+    
+    println!("{}", output[0]);
+    println!("{}", output[1]);
+    println!("{}", output[2]);
+    println!("{}", output[3]);
+    println!("{}", output[4]);
 }
