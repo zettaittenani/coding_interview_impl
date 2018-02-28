bffe46933a26c923baa2a60164bac0280b22b27e
zettaittenani <zettaittenani@gmail.com>
2018-02-28 22:13:28 +0900

    Why this mergesort isn't work

--- merge_sort/src/main.rs
+++ merge_sort/src/main.rs
@@ -1,44 +1,55 @@
-static mut sorted: Vec<i32> = vec![];
-
 fn main() {
-    let input: Vec<i32> = vec![5, 2, 6, 8, 3, 4, 1, 10, 9, 7];
-    let mut cloned_input: Vec<i32> = input.clone();
+    let input: Vec<usize> = vec![5, 2, 6, 8, 3, 4, 1, 10, 9, 7];
+    let mut cloned_input: Vec<usize> = input.clone();
+    let mut work: Vec<usize> = input.clone();
     let start_id: usize = 0;
     let end_id: usize = cloned_input.len();
 
-    _merge_sort(&mut cloned_input, start_id, end_id);
+    _merge_sort(&mut cloned_input, &mut work, start_id, end_id);
 
-    unsafe {
-        for i in 0..sorted.len() {
-            println!("{}", sorted[i]);
-        }
+    for i in 0..work.len() {
+        println!("{}", work[i]);
     }
 }
 
-fn _merge_sort(slice: &mut [i32], start_id: usize, end_id: usize) {
+fn _merge_sort(slice: &mut [usize], work: &mut [usize], start_id: usize, end_id: usize) {
     if end_id <= start_id {
         return
     }
 
-    let mut middle_id = (start_id + end_id) / 2;
-    let (mut left, mut right) = slice.split_at_mut(middle_id);
+    let middle_id: usize = (start_id + end_id) / 2;
+    _merge_sort(slice, work, start_id, middle_id);
+    _merge_sort(slice, work, (middle_id + 1), end_id);
 
-    _merge_sort(left, start_id, middle_id);
-    _merge_sort(right, middle_id + 1, end_id);
-
-    _merge(left, right, start_id, middle_id, end_id)
+    _merge(slice, work, start_id, middle_id, end_id)
 }
 
-fn _merge(left: &mut [i32], right: &mut [i32], start_id: usize, middle_id: usize, end_id: usize) {
-    for i in start_id..middle_id {
-        for j in (middle_id  + 1)..end_id {
-            unsafe {
-                if left[i] < right[j] {
-                    sorted[i] = left[i];
-                } else {
-                    sorted[i] = right[j];
-                }
-            }
+fn _merge(slice: &mut [usize], work: &mut [usize], start_id: usize, middle_id: usize, end_id: usize) {
+    let mut i: usize = 0;
+    let mut j: usize = middle_id + 1;
+    let mut k: usize = 0;
+
+    for m in start_id..middle_id {
+        work[k] = slice[m];
+        k += 1;
+    }
+
+    for n in (middle_id + 1)..end_id {
+        work[k] = slice[n];
+        k += 1;
+    }
+
+    for k in start_id..end_id {
+        if work[i] < work[j] {
+            slice[k] = work[i];
+            i += 1;
+        } else {
+            slice[k] = work[j];
+            j -= 1;
         }
+    }
+
+    for l in 0..slice.len() {
+        println!("i: {}, slice[i]: {}", l, slice[l]);
     }
 }
