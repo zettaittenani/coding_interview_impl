a60c67b484cc35c1c7da5f34871b1b96b64899c6
zettaittenani <zettaittenani@gmail.com>
2018-03-01 02:22:40 +0900

    Complete merge sort impl (not optimized)

--- merge_sort/src/main.rs
+++ merge_sort/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
     let input: Vec<usize> = vec![5, 2, 6, 8, 3, 4, 1, 10, 9, 7];
-    let mut cloned_input: Vec<usize> = input.clone();
-    let mut work: Vec<usize> = input.clone();
+    let mut output: Vec<usize> = input.clone();
+    let mut work: Vec<usize> = vec![0; 10];
     let start_id: usize = 0;
-    let end_id: usize = cloned_input.len() - 1;
+    let end_id: usize = output.len() - 1;
 
-    _merge_sort(&mut cloned_input, &mut work, start_id, end_id);
+    _merge_sort(&mut output, &mut work, start_id, end_id);
 
-    for i in 0..work.len() {
-        println!("{}", work[i]);
+    for i in 0..output.len() {
+        println!("{}", output[i]);
     }
 }
 
@@ -22,34 +22,37 @@ fn _merge_sort(slice: &mut [usize], work: &mut [usize], start_id: usize, end_id:
     _merge_sort(slice, work, start_id, middle_id);
     _merge_sort(slice, work, (middle_id + 1), end_id);
 
-    let mut i: usize = 0;
+    let mut i: usize = start_id;
+    let mut m: usize = start_id;
+    let mut n: usize = end_id;
 
-    for m in start_id..middle_id {
+    while m <= middle_id {
         work[i] = slice[m];
         i += 1;
+        m += 1;
     }
 
-    i = end_id;
+    i = middle_id + 1;
 
-    for n in (middle_id + 1)..end_id {
+    while n >= (middle_id + 1) {
         work[i] = slice[n];
-        i -= 1;
+        i += 1;
+        n -= 1;
     }
 
-    let mut j: usize = 0;
+    let mut j: usize = start_id;
     let mut k: usize = end_id;
+    let mut l: usize = start_id;
 
-    for l in start_id..end_id {
-        if work[j] < work[k] {
+    while l <= end_id {
+        if work[j] <= work[k] {
             slice[l] = work[j];
             j += 1;
+            l += 1;
         } else {
             slice[l] = work[k];
             k -= 1;
+            l += 1;
         }
-    }
-
-    for o in 0..slice.len() {
-        println!("i: {}, slice[i]: {}", o, slice[o]);
     }
 }
