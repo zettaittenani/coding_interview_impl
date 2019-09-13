87ae5e6c121185db29ef34538fff0488935a0931
zettaittenani <zettaittenani@gmail.com>
2019-09-13 10:30:37 +0000

    Revise quick sort

--- quick_sort/src/main.rs
+++ quick_sort/src/main.rs
@@ -1,41 +1,31 @@
 fn main() {
-    let input: Vec<usize> = vec![3, 2, 5, 9, 6, 10, 1, 4, 7, 8];
-    let mut mut_input: Vec<usize> = input.clone();
-    let start_id: usize = 0;
-    let end_id: usize = mut_input.len();
-
-    let output  = _quick_sort(&mut mut_input, start_id, end_id);
+    let mut input: Vec<u8> = vec![3, 2, 5, 9, 6, 10, 1, 4, 7, 8];
+    println!("{:?}", quick_sort(&mut input));
 }
 
-fn _quick_sort(slice: &mut [usize], start_id: usize, end_id: usize) -> &mut [usize] {
-    let slice_len: usize = slice.len();
-    if slice_len < 2 {
-        return slice
-    }
+fn quick_sort(slice: &mut Vec<u8>) -> Vec<u8> {
+    let len = slice.len();
+    if len == 1 { return slice.to_vec(); };
 
-    let pivot: usize = slice[start_id];
-    let mut left: Vec<usize> = vec![];
-    let mut right: Vec<usize> = vec![];
+    let pivot = slice[len / 2];
+    let mut split = 0;
 
-    for i in start_id..end_id {
-        if slice[i] <= pivot {
-            left.push(slice[i]);
-        } else {
-            right.push(slice[i]);
+    for i in 0..len {
+        for j in (0..len).rev() {
+            if i <= j {
+                if pivot <= slice[i] && slice[j] <= pivot {
+                    slice.swap(i, j);
+                    split = j;
+                }
+                // println!("{:?}", pivot);
+                // println!("{:?}", slice);
+            }
         }
     }
 
-    let left_len: usize = left.len();
-    let right_len: usize = right.len();
-
-    println!("{}, {}", left_len, right_len);
-
-    _quick_sort(&mut left, 0, left_len);
-    _quick_sort(&mut right, 0, right_len);
-
-    let mut ret: Vec<usize> = vec![];
-    ret.append(&mut left);
-    ret.append(&mut vec![pivot]);
-    ret.append(&mut right);
-    &mut ret
+    if split == 0 { return slice.to_vec(); };
+    let mut former: Vec<u8> = quick_sort(&mut slice[0..split].to_vec());
+    let mut latter: Vec<u8> = quick_sort(&mut slice[split..len].to_vec());
+    former.append(&mut latter);
+    former
 }
